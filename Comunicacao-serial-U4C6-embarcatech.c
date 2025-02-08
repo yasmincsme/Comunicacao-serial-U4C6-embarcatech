9a50cbd3c3a673ccb7258c3471d22d6b2b0c042e
Yasmin Cordeiro <ymeira@ecomp.uefs.br>
2025-02-08 19:00:15 -0300

    feat: criação de thread de leitura do caractere

--- Comunicacao-serial-U4C6-embarcatech.c
+++ Comunicacao-serial-U4C6-embarcatech.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "pico/stdlib.h"
+#include <pico/multicore.h>
 #include "hardware/i2c.h"
 #include "inc/ssd1306.h"
 #include "inc/font.h"
@@ -8,6 +10,24 @@
 #define I2C_SDA 14
 #define I2C_SCL 15
 #define endereco 0x3C
+
+ssd1306_t ssd; 
+bool cor = true;
+char character;
+
+void SSD1306_task() {
+	while (true) {
+    	// Atualiza o conteúdo do display com animações
+    	ssd1306_fill(&ssd, !cor); // Limpa o display
+    	ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
+    	ssd1306_draw_string(&ssd, "Caractere", 26, 10); // Desenha uma string
+    	ssd1306_draw_string(&ssd, "Escolhido", 26, 25); // Desenha uma string
+		ssd1306_draw_string(&ssd, &character, 60, 40); // Desenha uma string
+    	ssd1306_send_data(&ssd); // Atualiza o display
+
+    	sleep_ms(1000);
+  	}
+}
 
 void I2C_init() {
 	i2c_init(I2C_PORT, 400 * 1000);
@@ -27,26 +47,16 @@ void SSD1306_init(ssd1306_t* ssd) {
   	ssd1306_send_data(ssd);
 }
 
-int main()
-{
-	ssd1306_t ssd; 
-	bool cor = true;
+int main() {
 
 	stdio_init_all();
 	I2C_init();
 	SSD1306_init(&ssd);
+	multicore_launch_core1(SSD1306_task);
 	
-
-
   	while (true) {
-    	//cor = !cor;
-    	// Atualiza o conteúdo do display com animações
-    	ssd1306_fill(&ssd, !cor); // Limpa o display
-    	ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
-    	ssd1306_draw_string(&ssd, "abcdefghijkl", 8, 10); // Desenha uma string
-    	ssd1306_draw_string(&ssd, "mnopqrstuvwx", 8, 30); // Desenha uma string
-    	ssd1306_draw_string(&ssd, "yz 0123456789", 8, 48); // Desenha uma string      
-    	ssd1306_send_data(&ssd); // Atualiza o display
+
+		scanf(" %c", &character);
 
     	sleep_ms(1000);
   	}
