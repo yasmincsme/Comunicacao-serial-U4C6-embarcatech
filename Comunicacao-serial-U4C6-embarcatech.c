544cddccf9bec2d279e905852c0777b06e8f98e4
Yasmin Cordeiro <ymeira@ecomp.uefs.br>
2025-02-08 18:27:19 -0300

    feat: adição de caracteres minúsculos

--- Comunicacao-serial-U4C6-embarcatech.c
+++ Comunicacao-serial-U4C6-embarcatech.c
@@ -3,41 +3,51 @@
 #include "hardware/i2c.h"
 #include "inc/ssd1306.h"
 #include "inc/font.h"
+
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
 #define endereco 0x3C
 
+void I2C_init() {
+	i2c_init(I2C_PORT, 400 * 1000);
+
+	gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+	gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); 
+
+	gpio_pull_up(I2C_SDA);
+	gpio_pull_up(I2C_SCL);
+}
+
+void SSD1306_init(ssd1306_t* ssd) {
+	ssd1306_init(ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
+	ssd1306_config(ssd); // Configura o display
+  	ssd1306_send_data(ssd); // Envia os dados para o display
+	ssd1306_fill(ssd, false);
+  	ssd1306_send_data(ssd);
+}
+
 int main()
 {
-  // I2C Initialisation. Using it at 400Khz.
-  i2c_init(I2C_PORT, 400 * 1000);
+	ssd1306_t ssd; 
+	bool cor = true;
 
-  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
-  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
-  gpio_pull_up(I2C_SDA); // Pull up the data line
-  gpio_pull_up(I2C_SCL); // Pull up the clock line
-  ssd1306_t ssd; // Inicializa a estrutura do display
-  ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
-  ssd1306_config(&ssd); // Configura o display
-  ssd1306_send_data(&ssd); // Envia os dados para o display
+	stdio_init_all();
+	I2C_init();
+	SSD1306_init(&ssd);
+	
 
-  // Limpa o display. O display inicia com todos os pixels apagados.
-  ssd1306_fill(&ssd, false);
-  ssd1306_send_data(&ssd);
 
-  bool cor = true;
-  while (true)
-  {
-    cor = !cor;
-    // Atualiza o conteúdo do display com animações
-    ssd1306_fill(&ssd, !cor); // Limpa o display
-    ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
-    ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 10); // Desenha uma string
-    ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30); // Desenha uma string
-    ssd1306_draw_string(&ssd, "PROF WILTON", 15, 48); // Desenha uma string      
-    ssd1306_send_data(&ssd); // Atualiza o display
+  	while (true) {
+    	//cor = !cor;
+    	// Atualiza o conteúdo do display com animações
+    	ssd1306_fill(&ssd, !cor); // Limpa o display
+    	ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
+    	ssd1306_draw_string(&ssd, "abcdefghijkl", 8, 10); // Desenha uma string
+    	ssd1306_draw_string(&ssd, "mnopqrstuvwx", 8, 30); // Desenha uma string
+    	ssd1306_draw_string(&ssd, "yz 0123456789", 8, 48); // Desenha uma string      
+    	ssd1306_send_data(&ssd); // Atualiza o display
 
-    sleep_ms(1000);
-  }
+    	sleep_ms(1000);
+  	}
 }
--- inc/ssd1306.c
+++ inc/ssd1306.c
@@ -93,8 +93,6 @@ void ssd1306_fill(ssd1306_t *ssd, bool value) {
     }
 }
 
-
-
 void ssd1306_rect(ssd1306_t *ssd, uint8_t top, uint8_t left, uint8_t width, uint8_t height, bool value, bool fill) {
   for (uint8_t x = left; x < left + width; ++x) {
     ssd1306_pixel(ssd, x, top, value);
@@ -164,6 +162,9 @@ void ssd1306_draw_char(ssd1306_t *ssd, char c, uint8_t x, uint8_t y)
   }else  if (c >= '0' && c <= '9')
   {
     index = (c - '0' + 1) * 8; // Adiciona o deslocamento necessário
+  }else if(c >= 'a' && c <= 'z') 
+  {
+    index = (c - 'a' + 37) * 8;
   }
   
   for (uint8_t i = 0; i < 8; ++i)
